aa1dc99190610b59d304138f5a099054e8fe4243
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2025-03-24 22:22:16 +0600

    Add files via upload

---
+++ AddListItem.py
@@ -0,0 +1,13 @@
+#when append is used it means the new element is added in the last of the list
+
+d=["a","b","c","d"]
+d.append(10)
+print(d)
+
+#insert is used to insert element in specified position
+#insert(index,string)
+
+d=["a","b","c","d"]
+d.insert(1,"k")
+print(d)
+
